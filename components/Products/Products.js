19406b5c7d0d6baabcdb10846f836be9089fbb29
Alexey Mazheykin <mazheykin@icloud.com>
2020-12-01 17:55:32 +0300

    erg

---
+++ components/Products/Products.js
@@ -0,0 +1,27 @@
+class Products {
+
+    render() {
+        let htmlCatalog = ''
+        CATALOG.forEach(({id, img, name, price}) => {
+            htmlCatalog += `
+            <li class="products-item">
+                <span class="products-item__name">${name}</span>
+                 <img class="products-item__img"  src=${img} alt=${name}>
+                 <span class="products-item__price">${price}</span>
+                <button class="products-item__btn">Добавте в корзину</button>
+            </li>
+          `
+        });
+        const html = `
+        <ul class="products-container">
+            ${htmlCatalog}
+        </ul>
+        `;
+        ROOT__PRODUCTS.innerHTML = html
+    }
+
+}
+
+let prod = new Products();
+prod.render()
+
